c7eff8cd051f290f58e6af646efca1c8c94fbf49
Your Name <timaoz900@gmail.com>
2021-10-20 18:32:49 +0300

    add files

---
+++ dist/precache-manifest.bd2eed5dd4228f0bb9bdb8e6ad65da8f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5b2011e4963405b07e",
+    "url": "/TaskApp/css/app.5bb43ff1.css"
+  },
+  {
+    "revision": "76da561c9a50ae5620bb",
+    "url": "/TaskApp/css/chunk-1c89ad08.c1211139.css"
+  },
+  {
+    "revision": "6e30bac3c165239818b4",
+    "url": "/TaskApp/css/chunk-6717bb66.3ce3759c.css"
+  },
+  {
+    "revision": "a517875a8b5c249be9f5",
+    "url": "/TaskApp/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "b353848339f36dab156c1644ff2dc1c3",
+    "url": "/TaskApp/index.html"
+  },
+  {
+    "revision": "2b5b2011e4963405b07e",
+    "url": "/TaskApp/js/app.3e6397f2.js"
+  },
+  {
+    "revision": "76da561c9a50ae5620bb",
+    "url": "/TaskApp/js/chunk-1c89ad08.eaac9f78.js"
+  },
+  {
+    "revision": "6e30bac3c165239818b4",
+    "url": "/TaskApp/js/chunk-6717bb66.4036ecae.js"
+  },
+  {
+    "revision": "a517875a8b5c249be9f5",
+    "url": "/TaskApp/js/chunk-vendors.062df95e.js"
+  },
+  {
+    "revision": "82a3cd19efc53a2cd3f4aaaae50c5aaa",
+    "url": "/TaskApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TaskApp/robots.txt"
+  }
+]);
